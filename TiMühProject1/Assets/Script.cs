99a420cbb66fcdcd420c1693327a6fbb6cac3c13
Özge Cengizhan <74068525+ozgecengizhan@users.noreply.github.com>
2021-11-12 01:37:32 +0300

    Ti1
    Ti müh proje 1

---
+++ TiMühProject1/Assets/Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public float amplitude = 0.1f;
+    public float timeCoeff = 0.1f;
+    void Update()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        Vector3[] normals = mesh.normals;
+
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            vertices[i].y = amplitude * Mathf.Sin(((Time.time) + Mathf.Sqrt(Mathf.Abs(vertices[i].z * vertices[i].z) + (vertices[i].x * vertices[i].x))) * timeCoeff);
+        }
+
+        mesh.vertices = vertices;
+    }
+
+}
